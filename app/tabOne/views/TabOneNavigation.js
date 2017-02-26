2a14a94c1509b2ce93c02c3fba1c4d39b7d095bb
Monte9 <Monte9@users.noreply.github.com>
2017-02-26 03:23:12 -0800

    added sendbird connect; setup connect logic

--- app/tabOne/views/TabOneNavigation.js
+++ app/tabOne/views/TabOneNavigation.js
@@ -1,4 +1,14 @@
 import React from 'react'
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  AppState,
+  Platform,
+  Keyboard,
+  KeyboardAvoidingView
+} from 'react-native'
 
 import { addNavigationHelpers } from 'react-navigation'
 import { NavigatorTabOne } from '../navigationConfiguration'
@@ -6,6 +16,10 @@ import { NavigatorTabOne } from '../navigationConfiguration'
 import { connect } from 'react-redux'
 
 import Icon from 'react-native-vector-icons/FontAwesome'
+import SendBird from 'sendbird'
+
+import {APP_ID} from '../../consts'
+var sb = null;
 
 const mapStateToProps = (state) => {
  return {
@@ -19,6 +33,61 @@ class TabOneNavigation extends React.Component {
       label: 'Message',
       icon: ({ tintColor }) => <Icon size={ 20 } name={ 'comments' } color={ tintColor }/>
     }
+  }
+
+  componentDidMount() {
+    sb = new SendBird({appId: APP_ID});
+    AppState.addEventListener('change', function(currentAppState){
+      if (currentAppState === 'active') {
+        console.log('foreground');
+        sb.setForegroundState();
+      } else if (currentAppState === 'background') {
+        console.log('background');
+        sb.setBackgroundState();
+      }
+    });
+
+    // var Notifications = require('react-native-push-notification');
+    // Notifications.configure({
+    //     onRegister: function(token) {
+    //         if (Platform.OS === 'ios') {
+    //           sb.registerAPNSPushTokenForCurrentUser(token['token'], function(result, error){
+    //             console.log("registerAPNSPushTokenForCurrentUser");
+    //             console.log(result);
+    //           });
+    //         } else {
+    //           sb.registerGCMPushTokenForCurrentUser(token['token'], function(result, error){
+    //             console.log("registerAPNSPushTokenForCurrentUser");
+    //             console.log(result);
+    //           });
+    //         }
+    //     },
+    //
+    //     onNotification: function(notification) {
+    //         console.log( 'NOTIFICATION:', notification );
+    //     },
+    //
+    //     // ANDROID ONLY: GCM Sender ID (optional - not required for local notifications, but is need to receive remote push notifications)
+    //     senderID: "984140644677",
+    //
+    //     // IOS ONLY (optional): default: all - Permissions to register.
+    //     permissions: {
+    //         alert: true,
+    //         badge: true,
+    //         sound: true
+    //     },
+    //
+    //     // Should the initial notification be popped automatically
+    //     // default: true
+    //     popInitialNotification: true,
+    //
+    //     /**
+    //       * (optional) default: true
+    //       * - Specified if permissions (ios) and token (android and ios) will requested or not,
+    //       * - if not, you must call PushNotificationsHandler.requestPermissions() later
+    //       */
+    //     requestPermissions: true,
+    // });
   }
 
   render(){
--- app/tabOne/views/TabOneScreenOne.js
+++ app/tabOne/views/TabOneScreenOne.js
@@ -1,27 +1,137 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import {
+  View,
+  Text,
+  AppState,
+  Platform,
+  Keyboard,
+  KeyboardAvoidingView
+} from 'react-native'
 
-import { Button } from 'react-native-elements'
+import { Icon } from 'react-native-elements'
+
+import SendBird from 'sendbird'
+var sb = null;
+
+const LoginView = Platform.select({
+  ios: () => KeyboardAvoidingView,
+  android: () => View,
+})();
 
 export default class TabOneScreenOne extends Component {
   static navigationOptions = {
     title: 'Message',
   }
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      userId: 'monte9',
+      username: 'monte9',
+      connectLabel: 'CONNECT',
+      buttonDisabled: true,
+      errorMessage: ''
+    };
+    this._onPressConnect = this._onPressConnect.bind(this);
+    // this._onPressOpenChannel = this._onPressOpenChannel.bind(this);
+    // this._onPressGroupChannel = this._onPressGroupChannel.bind(this)
+  }
+
+  _onPressConnect() {
+    Keyboard.dismiss();
+
+    if (!this.state.buttonDisabled) {
+      this.props.navigation.navigate('TabOneScreenTwo')
+      // this._onPressDisconnect();
+      return;
+    }
+
+    sb = SendBird.getInstance();
+    var _SELF = this;
+    sb.connect(_SELF.state.userId, function (user, error) {
+      if (error) {
+        _SELF.setState({
+          userId: '',
+          username: '',
+          errorMessage: 'Login Error'
+        });
+        console.log(error);
+        return;
+      }
+
+      // if (Platform.OS === 'ios') {
+      //   if (sb.getPendingAPNSToken()){
+      //     sb.registerAPNSPushTokenForCurrentUser(sb.getPendingAPNSToken(), function(result, error){
+      //       console.log("APNS TOKEN REGISTER AFTER LOGIN");
+      //       console.log(result);
+      //     });
+      //   }
+      // } else {
+      //   if (sb.getPendingGCMToken()){
+      //     sb.registerGCMPushTokenForCurrentUser(sb.getPendingGCMToken(), function(result, error){
+      //       console.log("GCM TOKEN REGISTER AFTER LOGIN");
+      //       console.log(result);
+      //     });
+      //   }
+      // }
+
+      sb.updateCurrentUserInfo(_SELF.state.username, '', function(response, error) {
+        _SELF.setState({
+          buttonDisabled: false,
+          connectLabel: 'DISCONNECT',
+          errorMessage: ''
+        });
+      });
+    });
+  }
+
+  // _onPressOpenChannel() {
+  //   this.props.navigator.push({name: 'openChannel'});
+  // }
+  //
+  // _onPressGroupChannel() {
+  //   this.props.navigator.push({name: 'groupChannel'});
+  // }
+
+  _onPressDisconnect() {
+    sb.disconnect();
+    this.setState({
+      userId: '',
+      username: '',
+      errorMessage: '',
+      buttonDisabled: true,
+      connectLabel: 'CONNECT'
+    });
+  }
+
   render(){
     return(
       <View style={{
         flex:1,
-        alignItems:'center',
         backgroundColor: '#1C7CF3',
-        justifyContent:'center'
+        alignItems:'center',
+        justifyContent:'center',
       }}>
-      <Button
-        iconRight
-        buttonStyle={{ borderRadius:30, backgroundColor: '#1BA1F7' }}
-        icon={{ name: 'comments-o', type: 'font-awesome', size: 30 }}
-        title='EXPLORE MESSAGES'
-        onPress={ () => this.props.navigation.navigate('TabOneScreenTwo') } />
+        <View style={{width: 300, paddingBottom: 20}}>
+          <Text style={{color: 'white', fontSize: 22, fontWeight: '500', textAlign: 'center'}}>
+            {this.state.buttonDisabled ? 'Click the button below to view your messages' : 'CONNECTED! \n Click below to continue'}
+          </Text>
+        </View>
+        <Icon
+          raised
+          loading={true}
+          name={ this.state.buttonDisabled ? 'bolt' : 'chevron-right' }
+          size={35}
+          type='font-awesome'
+          color='#61E02B'
+          containerStyle={{backgroundColor: 'white'}}
+          onPress={this._onPressConnect}
+          onLongPress={this._onPressDisconnect.bind(this)}/>
+          <View style={{width: 200, paddingTop: 20}}>
+            <Text style={{color: 'white', fontSize: 18, fontWeight: '500', textAlign: 'center'}}>
+              {!this.state.buttonDisabled && '(Long press above to disconnect)'}
+            </Text>
+          </View>
       </View>
     )
   }
